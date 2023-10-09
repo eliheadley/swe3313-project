3d9fb16299c906ed8dbd8aaefc6fa3830c4cc972
eliheadley <eliheadley@gmail.com>
2023-10-09 16:33:54 -0400

    basic ui connected to database

---
+++ src/Database.java
@@ -0,0 +1,49 @@
+import java.sql.*;
+
+public class Database{
+    private static final String URL = "jdbc:hsqldb:hsql://localhost/";
+    private Connection conn = null;
+    private String sql = "";
+    private PreparedStatement pstmt = null;
+    private ResultSet rs = null;
+
+    public String[] getUser(String userName, String Password){
+        sql = "SELECT * FROM customer WHERE PhoneNumber = ? AND Password = ?";
+        String[] querry = null;
+        try{
+            Class.forName("org.hsqldb.jdbc.JDBCDriver");
+            conn = DriverManager.getConnection(URL, "SA", "");            
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, userName);
+            pstmt.setString(2, Password);
+            rs = pstmt.executeQuery();
+            querry = new String[6];
+            while(rs.next()){
+                for(int i=0; i<querry.length; i++){
+                    querry[i] = rs.getString(i+1);
+                }                
+            }
+        }catch(SQLException sqle){
+            return null;            
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+        return querry;
+    }
+
+    public void insertUserName(String userName){
+        sql = "INSERT INTO customer VALUES(\'?\') ";
+        try{
+            Class.forName("org.hsqldb.jdbc.JDBCDriver");
+            conn = DriverManager.getConnection(URL, "SA", "");            
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, userName);
+            rs = pstmt.executeQuery();            
+        }catch(SQLException sqle){
+            System.out.println("Querry failed");            
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,79 @@
+import java.awt.event.*;
+import javax.swing.*;
 class Main{
+    static String phoneNumber;
+    static String password;
+    static String[] user;
     public static void main(String[] args){
-        System.out.println("Hello world");
+        Database db = new Database();
+
+        JFrame f = new JFrame();
+        //Create text fields to input data
+        final JTextField txtIn1 = new JTextField();
+        txtIn1.setBounds(105, 25, 170, 20);
+        final JTextField txtIn2 = new JTextField();
+        txtIn2.setBounds(105, 75, 170, 20);
+        final JTextField txtOut1 = new JTextField();
+        txtOut1.setBounds(105, 250, 170, 20);
+        final JTextField txtOut2 = new JTextField();
+        txtOut2.setBounds(105, 300, 170, 20);
+        final JTextField txtOut3 = new JTextField();
+        txtOut3.setBounds(105, 350, 170, 20);
+        final JTextField txtOut4 = new JTextField();
+        txtOut4.setBounds(105, 400, 170, 20);
+
+        
+        //Create login button
+        JButton b1 = new JButton("Login");
+        b1.setBounds(145,125,120,30);//x axis, y axis, width, height
+
+        //Creat clear button
+        JButton b2 = new JButton("Clear");
+        b2.setBounds(165,180,80,30);
+
+    
+        //Login button action
+        b1.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                phoneNumber = txtIn1.getText();
+                password = txtIn2.getText();
+                user = db.getUser(phoneNumber, password);
+                if(user[0] != null){
+                    txtOut1.setText(user[2]);
+                    txtOut2.setText(user[3]);
+                    txtOut3.setText(user[4]);
+                    txtOut4.setText(user[5]);
+                }else{
+                    txtIn1.setText("Invalid Username or Password");
+                    txtIn2.setText("Invalid Username or Password");
+                }               
+            }
+        }
+        );
+
+        //Clear button action
+        b2.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                txtIn1.setText("");
+                txtIn2.setText("");
+                txtOut1.setText("");
+                txtOut2.setText("");
+                txtOut3.setText("");
+                txtOut4.setText("");
+            }
+        });
+
+        //add all elements to JFrame
+        f.add(b1);
+        f.add(b2);
+        f.add(txtIn1);
+        f.add(txtIn2);
+        f.add(txtOut1);
+        f.add(txtOut2);
+        f.add(txtOut3);
+        f.add(txtOut4);
+        f.setSize(400, 500);
+        f.setLayout(null);
+        f.setVisible(true);
     }
 }

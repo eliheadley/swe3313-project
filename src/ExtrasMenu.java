3e7f3bb17afb7543603a1303fbbdad6b875fe6e3
eliheadley <eliheadley@gmail.com>
2023-11-22 22:10:57 -0500

    working extras menu and order summary

--- src/ExtrasMenu.java
+++ src/ExtrasMenu.java
@@ -20,40 +20,58 @@ public class ExtrasMenu extends Page implements ActionListener {
         checkBoxFont = new Font("Impact", Font.PLAIN, 24);
 
         rightPanel = new JPanel(new GridLayout(2, 1)); // Use a 1x3 grid layout
-        rightPanel.setBounds(8*w5p, 7*h5p, 900, 300);
+        rightPanel.setBounds(8*w5p, 7*h5p, 900, 400);
 
         //sub panels
         drinkChoices = new JPanel(new GridLayout(1,2));
         drinkChoices.setBorder(BorderFactory.createEmptyBorder(20, 10, 10, 10)); // Add some space
+        drinkChoices.setPreferredSize(new Dimension(100, 150));
+        drinkChoices.setBackground(Color.decode("#cccccc"));
         //create sub panels for first drink column and second drink column
         pepsi_oragange = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        pepsi_oragange.setBackground(Color.decode("#cccccc"));
         pepsi_oragange.setLayout(new BoxLayout(pepsi_oragange, BoxLayout.Y_AXIS)); // Vertical layout
         rtBeer_lemonade = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        rtBeer_lemonade.setBackground(Color.decode("#cccccc"));
         rtBeer_lemonade.setLayout(new BoxLayout(rtBeer_lemonade, BoxLayout.Y_AXIS)); // Vertical layout
 
         drinkSizes = new JPanel(new FlowLayout(FlowLayout.CENTER));
         drinkSizes.setLayout(new BoxLayout(drinkSizes, BoxLayout.Y_AXIS)); // Vertical layout
         drinkSizes.setBorder(BorderFactory.createEmptyBorder(20, 10, 10, 10));
+        drinkSizes.setBackground(Color.decode("#cccccc"));
 
-        drinksLabel = new JLabel("Drinks");
-        this.add(drinksLabel);
+        drinksLabel = new JLabel("Drinks:");
+        drinksLabel.setFont(font1);
+        drinksLabel.setBounds((8*w5p)+20, 5*h5p, 300, 100);
 
         // create check boxes for drink menu
         pepsi = new JCheckBox("Pepsi");
+        pepsi.setFont(checkBoxFont);
         dietPepsi = new JCheckBox("Diet Pepsi");
+        dietPepsi.setFont(checkBoxFont);
         orange = new JCheckBox("Orange");
+        orange.setFont(checkBoxFont);
         dietOrange = new JCheckBox("Diet Orange");
+        dietOrange.setFont(checkBoxFont);
         rootBeer = new JCheckBox("Root Beer");
+        rootBeer.setFont(checkBoxFont);
         dietRB = new JCheckBox("Diet Root Beer");
+        dietRB.setFont(checkBoxFont);
         sierraMist = new JCheckBox("Sierra Mist");
+        sierraMist.setFont(checkBoxFont);
         lemonade = new JCheckBox("Lemonade");
+        lemonade.setFont(checkBoxFont);
 
-        drinkSizeLabel = new JLabel("Size");
+        drinkSizeLabel = new JLabel("Size:");
+        drinkSizeLabel.setFont(font1);
         drinkSizes.add(drinkSizeLabel);
 
         small = new JCheckBox("Small");
+        small.setFont(checkBoxFont);
         medium = new JCheckBox("Medium");
+        medium.setFont(checkBoxFont);
         large = new JCheckBox("Large");
+        large.setFont(checkBoxFont);
         
         // add drink choice check boxes to panel
         pepsi_oragange.add(pepsi);
@@ -79,16 +97,20 @@ public class ExtrasMenu extends Page implements ActionListener {
         // create left panel to hold sides
         leftPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
         leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS)); // Vertical layout
-        leftPanel.setBounds(4*w5p, 6*h5p, 150, 400);
+        leftPanel.setBounds(2*w5p, 6*h5p, 350, 400);
 
         // create lable for sides and add to panel
-        sidesLabel = new JLabel("Sides");
+        sidesLabel = new JLabel("Sides:");
+        sidesLabel.setFont(font1);
         leftPanel.add(sidesLabel);
 
         // create checkboxes for side options
-        breadSticks = new JCheckBox("Bread Sticks");
-        breadSticksBites = new JCheckBox("Bread Sticks Bites");
-        cookie = new JCheckBox("Big Chocolate Chip Cookie");
+        breadSticks = new JCheckBox("Bread Sticks $4");
+        breadSticks.setFont(checkBoxFont);
+        breadSticksBites = new JCheckBox("Bread Sticks Bites $2");
+        breadSticksBites.setFont(checkBoxFont);
+        cookie = new JCheckBox("Big Chocolate Chip Cookie $4");
+        cookie.setFont(checkBoxFont);
 
         // checkboxes to left panel
         leftPanel.add(breadSticks);
@@ -118,8 +140,25 @@ public class ExtrasMenu extends Page implements ActionListener {
         title.setFont(titleFont);
         title.setBounds((9*w5p), h5p, 500, 60);
 
+        // set background for all elements
+        pepsi.setBackground(Color.decode("#cccccc"));
+        dietPepsi.setBackground(Color.decode("#cccccc"));
+        orange.setBackground(Color.decode("#cccccc"));
+        dietOrange.setBackground(Color.decode("#cccccc"));
+        rootBeer.setBackground(Color.decode("#cccccc"));
+        dietRB.setBackground(Color.decode("#cccccc"));
+        sierraMist.setBackground(Color.decode("#cccccc"));
+        lemonade.setBackground(Color.decode("#cccccc"));
+        small.setBackground(Color.decode("#cccccc"));
+        medium.setBackground(Color.decode("#cccccc"));
+        large.setBackground(Color.decode("#cccccc"));
+        breadSticks.setBackground(Color.decode("#cccccc"));
+        breadSticksBites.setBackground(Color.decode("#cccccc"));
+        cookie.setBackground(Color.decode("#cccccc"));
+        rightPanel.setBackground(Color.decode("#cccccc"));
+        leftPanel.setBackground(Color.decode("#cccccc"));
 
-
+        // add all the elements
         this.getContentPane().setLayout(null);
         this.add(title);
         this.add(backButton);
@@ -127,6 +166,7 @@ public class ExtrasMenu extends Page implements ActionListener {
         this.add(addToOrderButton);
         this.add(rightPanel);
         this.add(leftPanel);
+        this.add(drinksLabel);
         this.setVisible(false);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setExtendedState(JFrame.MAXIMIZED_BOTH);
@@ -138,7 +178,6 @@ public class ExtrasMenu extends Page implements ActionListener {
         if(visible){
             this.setVisible(true);
          }
- 
     }
 
     @Override
--- src/OrderSummary.java
+++ src/OrderSummary.java
@@ -5,8 +5,9 @@ import javax.swing.*;
 import javax.swing.border.Border;
 
 public class OrderSummary extends Page implements ActionListener{
-    JLabel title, pizzaLabel, extrasLabel, qty1, each1, total1, space, line;
-    JButton back, checkOut, edit1, edit2, edit3;
+    JLabel title, pizzaLabel, extrasLabel, qty1, each1, total1, line1, line2, 
+    qty2, each2, total2;
+    JButton back, checkOut, edit1, edit2, edit3, inc1, inc2, inc3, dec1, dec2, dec3;
     Font buttonFont, textFont, titleFont;
 
     public OrderSummary(){
@@ -19,11 +20,11 @@ public class OrderSummary extends Page implements ActionListener{
         title.setBounds((7*w5p), h5p, 700, 60);
         title.setFont(titleFont);
 
-        //Make separator line
-        line = new JLabel();
-        line.setBounds(2*w5p, 6*h5p, 1200, 2);
-        Border border = BorderFactory.createLineBorder(Color.BLACK, 10);
-        line.setBorder(border);
+        //Make first separator line
+        line1 = new JLabel();
+        line1.setBounds(2*w5p, 6*h5p, 1200, 2);
+        Border border1 = BorderFactory.createLineBorder(Color.BLACK, 10);
+        line1.setBorder(border1);
 
         // make pizza label for pizza order summary
         pizzaLabel = new JLabel("Pizza");
@@ -42,7 +43,87 @@ public class OrderSummary extends Page implements ActionListener{
         total1 = new JLabel("Total");
         total1.setFont(textFont);
         total1.setBounds((15*w5p)+15, (5*h5p)+10, 50, 30);
+
+        // make buttons for first summary
+        edit1 = new JButton("Edit");
+        edit1.setBounds((10*w5p)-10, (6*h5p)+20, 70, 30);
+        edit1.setBackground(Color.decode("#e06666"));
+        edit1.setFont(buttonFont);
+        edit1.addActionListener(this);
         
+        inc1 = new JButton("+");
+        inc1.setBounds((11*w5p)+10, (6*h5p)+20, 50, 30);
+        inc1.setBackground(Color.decode("#e06666"));
+        inc1.setFont(buttonFont);
+        inc1.addActionListener(this);
+
+        dec1 = new JButton("-");
+        dec1.setBounds((12*w5p)-10, (6*h5p)+20, 50, 30);
+        dec1.setBackground(Color.decode("#e06666"));
+        dec1.setFont(buttonFont);
+        dec1.addActionListener(this);
+
+        //Make second separator line
+        line2 = new JLabel();
+        line2.setBounds(2*w5p, 13*h5p, 1200, 2);
+        Border border2 = BorderFactory.createLineBorder(Color.BLACK, 10);
+        line2.setBorder(border2);
+        
+        // make extras label for extras order summary
+        extrasLabel = new JLabel("Extras");
+        extrasLabel.setFont(textFont);
+        extrasLabel.setBounds((2*w5p), (12*h5p)+10, 70, 30);
+
+        // make qty, each, and total labels
+        qty2 = new JLabel("Qty");
+        qty2.setFont(textFont);
+        qty2.setBounds((13*w5p), (12*h5p)+10, 50, 30);
+
+        each2 = new JLabel("Each");
+        each2.setFont(textFont);
+        each2.setBounds((14*w5p), (12*h5p)+10, 50, 30);
+
+        total2 = new JLabel("Total");
+        total2.setFont(textFont);
+        total2.setBounds((15*w5p)+15, (12*h5p)+10, 50, 30);
+
+        // make buttons for second summary
+        edit2 = new JButton("Edit");
+        edit2.setBounds((10*w5p)-10, (13*h5p)+20, 70, 30);
+        edit2.setBackground(Color.decode("#e06666"));
+        edit2.setFont(buttonFont);
+        edit2.addActionListener(this);
+        
+        inc2 = new JButton("+");
+        inc2.setBounds((11*w5p)+10, (13*h5p)+20, 50, 30);
+        inc2.setBackground(Color.decode("#e06666"));
+        inc2.setFont(buttonFont);
+        inc2.addActionListener(this);
+
+        dec2 = new JButton("-");
+        dec2.setBounds((12*w5p)-10, (13*h5p)+20, 50, 30);
+        dec2.setBackground(Color.decode("#e06666"));
+        dec2.setFont(buttonFont);
+        dec2.addActionListener(this);
+
+        // make buttons for third summary
+        edit3 = new JButton("Edit");
+        edit3.setBounds((10*w5p)-10, (16*h5p)+20, 70, 30);
+        edit3.setBackground(Color.decode("#e06666"));
+        edit3.setFont(buttonFont);
+        edit3.addActionListener(this);
+        
+        inc3 = new JButton("+");
+        inc3.setBounds((11*w5p)+10, (16*h5p)+20, 50, 30);
+        inc3.setBackground(Color.decode("#e06666"));
+        inc3.setFont(buttonFont);
+        inc3.addActionListener(this);
+
+        dec3 = new JButton("-");
+        dec3.setBounds((12*w5p)-10, (16*h5p)+20, 50, 30);
+        dec3.setBackground(Color.decode("#e06666"));
+        dec3.setFont(buttonFont);
+        dec3.addActionListener(this);
         
 
         // create back button
@@ -61,15 +142,21 @@ public class OrderSummary extends Page implements ActionListener{
         // add buttons to frame
         this.add(back);
         this.add(checkOut);
-        // add labels to the frame
+        // add labels for first summary
         this.add(title);
-        this.add(line);
+        this.add(line1);
         this.add(pizzaLabel); this.add(qty1); this.add(each1); this.add(total1);
+        this.add(edit1); this.add(inc1); this.add(dec1);
+        // add labels for second summary
+        this.add(line2);
+        this.add(extrasLabel); this.add(qty2); this.add(each2); this.add(total2);
+        this.add(edit2); this.add(inc2); this.add(dec2);
+        // add buttons for thirds summary
+         this.add(edit3); this.add(inc3); this.add(dec3);
 
         // set the layout for the frame
         this.getContentPane().setLayout(null);
         setExtendedState(JFrame.MAXIMIZED_BOTH);
-        setLayout(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(false);
         this.getContentPane().setBackground(Color.decode("#cccccc"));
@@ -88,7 +175,7 @@ public class OrderSummary extends Page implements ActionListener{
             new PizzaMenu().showPizzaMenu(true);
             this.dispose();
          // check out button
-        }else if(e.getActionCommand().equals("Back")){
+        }else if(e.getActionCommand().equals("Check Out")){
             this.dispose();
         }
      }
